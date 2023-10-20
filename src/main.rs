9ee2fb6aeb0b54e4e88c4aec9796ff26e3977fd6
codescalper <msfunbook@gmail.com>
2023-10-20 11:05:09 +0530

    Serialized article instance into json string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+
+struct Paragraph {
+    name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Article {
+    article: String,
+    author: String,
+    paragraph: Vec<Paragraph>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let article = Article {
+        article: String::from("How to work with json in rust"),
+        author: String::from("Mayank Singh"),
+        paragraph: vec![
+            Paragraph {
+                name: String::from("First Sentence"),
+            },
+            Paragraph {
+                name: String::from("Second Sentence"),
+            },
+            Paragraph {
+                name: String::from("Third Sentence"),
+            },
+        ],
+    };
+
+    let json = serde_json::to_string_pretty(&article).unwrap();
+
+    println!("The json is {:?}", json)
 }
